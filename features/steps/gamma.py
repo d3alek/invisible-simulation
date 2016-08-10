e361d00dae52f4a42defbddb0ee03415deeb9360
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-08-10 15:22:55 +0100

    Fixing behavioural tests after SkyModel refactoring

--- features/steps/gamma.py
+++ features/steps/gamma.py
@@ -25,6 +25,7 @@ def step_impl(context, direction):
 
 @then('gamma is {expected_gamma:d}')
 def step_impl(context, expected_gamma):
-    gamma_degrees = np.floor(np.rad2deg(SkyModel(context.sun, context.observed).get_gamma()))
+    observed_radians = [*map(np.deg2rad, context.observed)]
+    gamma_degrees = np.floor(np.rad2deg(SkyModel().with_sun_at_degrees(context.sun).get_gamma(observed_radians)))
     assert np.isclose(gamma_degrees, expected_gamma), "Expected %d, actual %d" % (expected_gamma, gamma_degrees)
 
