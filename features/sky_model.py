Fix for polarization vector's angle calculation. 2D projection (sky_map) still does not work as expected.

features/sky_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import ipdb
 # using the horizontal celestrial coordinate system https://en.wikipedia.org/wiki/Horizontal_coordinate_system
 # this means we have two coordinates: 
 # - altitude - elevation (0-90)
@@ -52,6 +53,37 @@ def warn_if_looks_like_degrees(radians):
     if max(radians) > 2*np.pi:
        print("It is possible you are passing degrees to a function that expects radians. Radians passed: ", radians) 
 
+def project(vector, plane_normal):
+    """ Projects a vector onto a plane given by its normal
+    http://www.euclideanspace.com/maths/geometry/elements/plane/lineOnPlane/
+
+    >>> round_print(project([1, 1, 0], [0, 0, 1]))
+    [ 1.  1.  0.]
+    >>> round_print(project([1, 0, 0], [1, 0, 0]))
+    [ 0.  0.  0.]
+    >>> round_print(project([1, 1, 0], [1, 0, 0]))
+    [ 0.  1.  0.]
+    >>> round_print(project([1, 0, 0], [0, 1, 1]))
+    [ 1.  0.  0.]
+    """
+    plane_normal = unit_vector(plane_normal)
+    return np.cross(np.cross(plane_normal, vector), plane_normal)
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray(axis)
+    axis = axis/np.sqrt(np.dot(axis, axis))
+    a = np.cos(theta/2.0)
+    b, c, d = -axis*np.sin(theta/2.0)
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
+                     [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
+                     [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+
 class SkyModelGenerator:
     zenith = np.array((np.pi/2, 0))
 
@@ -96,11 +128,33 @@ class SkyModelGenerator:
         return angle_between(v1, v2)
 
     # http://www.math.ucla.edu/~ronmiech/Calculus_Problems/32A/chap11/section4/708d23/708_23.html
+
     def get_angle(self, point_radians):
         angle_vector = self.get_angle_vector(point_radians)
-        angle = np.arctan2(angle_vector[2], angle_vector[0])
+
+        cartesian_observed = to_cartesian(point_radians)
+        tangent_plane_normal = unit_vector(cartesian_observed)
+        projected_angle_vector = project(angle_vector, tangent_plane_normal)
+        
+        # following http://stackoverflow.com/questions/6802577/python-rotation-of-3d-vector
+	
+        normal = (0, 0, 1)
+        rotation_axis = np.cross(normal, tangent_plane_normal)
+        matrix = rotation_matrix(rotation_axis, angle_between(tangent_plane_normal, normal))
+        assert np.allclose(np.dot(tangent_plane_normal, matrix), normal)
+
+        rotated_angle_vector = np.dot(projected_angle_vector, matrix)
+
+        #angle = np.arctan2(rotated_angle_vector[1], rotated_angle_vector[0])
+        angle = angle_between(rotated_angle_vector, (1, 0, 0))
         if angle < 0:
             angle += np.pi
+
+        if angle > 180:
+            angle -= np.pi/2
+
+        assert angle >= 0 and angle <= 180
+
         return angle
     
     # useful for 3d plotting
@@ -116,10 +170,6 @@ class SkyModelGenerator:
         azimuths, altitudes = np.meshgrid(observed_azimuths, observed_altitudes)
 
         cartesian = map(to_cartesian, zip(altitudes, azimuths))
-
-        #x = np.cos(azimuths) * np.sin(altitudes)
-        #y = np.sin(azimuths) * np.sin(altitudes)
-        #z = np.cos(altitudes)
 
         angle_vectors = np.empty(azimuths.shape, dtype=list)
         angles = np.empty(azimuths.shape)

sky_map.py
@@ -23,9 +23,6 @@ RADIUS = 400
 LATITUDE_SEVILLA = 37.366123
 LONGITUDE_SEVILLA = -5.996422
 
-
-
-
 WIDTH = 2*RADIUS + 100
 HEIGHT = WIDTH
 
@@ -36,6 +33,7 @@ pygame.display.set_caption('Sky Map')
 
 blackColor = pygame.Color(0, 0, 0)
 whiteColor = pygame.Color(255, 255, 255)
+redColor = pygame.Color(255, 0, 0)
 yellowColor = pygame.Color(255, 255, 0)
 
 sun_at = EAST
@@ -56,14 +54,17 @@ def cartesian_to_polar(cartesian):
     r = np.sqrt(x*x + y*y)
     azimuth = np.arctan2(y, x)
 
-    return radius_to_altitude(r) + np.pi/2, azimuth % (2*np.pi)
+    return -r * np.pi/2 + np.pi/2, azimuth % (2*np.pi)
 
 def polar(sky_map_coordinates):
     """Opposite of cartesian2d. For some reason this doctest does not execute
 
-    >>> p = (np.pi/2, 3*np.pi/2)
-    >>> polar(cartesian2d(p))
-    False
+    >>> p = (1.5, 4.7)
+    >>> print(np.round((polar(cartesian2d(p))), 1))
+    [ 1.5  4.7]
+    >>> p = (0.5, 0.3)
+    >>> print(np.round((polar(cartesian2d(p))), 1))
+    [ 0.5  0.3]
     """
    
     non_centered = (sky_map_coordinates - CENTER) / np.array([RADIUS, RADIUS])
@@ -94,16 +95,23 @@ def draw_arrow(angle_rad, pos, width=1):
     arrow.set_colorkey(blackColor)
 
     # adding np.pi/2 empirically determined when comparing to 3D plot (from reproduce_wiki_plots.py)
-    rotated_arrow = pygame.transform.rotate(arrow, np.rad2deg(angle_rad+np.pi/2)) 
+    rotated_arrow = pygame.transform.rotate(arrow, np.rad2deg(angle_rad)) 
     rect = rotated_arrow.get_rect(center=pos)
     windowSurfaceObj.blit(rotated_arrow, rect)
+
+    fontObj = pygame.font.Font('freesansbold.ttf', 13)
+    renderedFont = fontObj.render(str(np.int8(np.round(np.rad2deg(angle_rad), 0))), False, redColor)
+    rect = renderedFont.get_rect(center=pos+5)
+    windowSurfaceObj.blit(renderedFont, rect)
 
 def draw_angles():
     sky_model = SkyModelGenerator().with_sun_at(sun_at).generate(observed_altitudes, observed_azimuths)
     for index_altitude, altitude in enumerate(observed_altitudes):
         for index_azimuth, azimuth in enumerate(observed_azimuths):
             pos = cartesian2d((altitude, azimuth))
-            draw_arrow(sky_model.angles[index_altitude, index_azimuth], pos, int(1+5*sky_model.degrees[index_altitude, index_azimuth]))
+            angle = sky_model.angles[index_altitude, index_azimuth]
+            degree = sky_model.degrees[index_altitude, index_azimuth]
+            draw_arrow(angle, pos, int(1+5*degree))
 
 date = datetime.date.today()
 def calculate_sunrise_sunset_times():
@@ -150,7 +158,7 @@ mouse_down = False
 def print_angle_and_degree_at(sky_map_coordinates):
     observed = polar(sky_map_coordinates)
     sky_model_generator = SkyModelGenerator().with_sun_at(sun_at)
-    print("Angle: %f Degree %f" % (np.rad2deg(sky_model_generator.get_angle(observed)), sky_model_generator.get_degree(observed)))
+    print("Observed: %s Angle: %f Degree %f" % (np.rad2deg(observed), np.rad2deg(sky_model_generator.get_angle(observed)), sky_model_generator.get_degree(observed)))
 
 if __name__ == "__main__":
     import doctest