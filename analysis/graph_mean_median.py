d6fd112db1865bad59ee3fb8038277703883d5f0
Aleksandar Kodzhabashev <akodzhabashev@gmail.com>
2016-10-31 15:28:39 +0000

    Added a script to plot mean and median prediction results data.
    The data is first processed manually. Delete the prediction results
    table using vim. Then do something like cat <file> | awk '{print $1, $4, $6}'

---
+++ analysis/graph_mean_median.py
@@ -0,0 +1,25 @@
+from operator import itemgetter
+import csv
+import sys
+from matplotlib import pyplot as plt
+import datetime
+
+def plot_data(file_name):
+    global corrected
+    data = [*csv.reader(open(file_name), delimiter=" ")]
+    data = [*map(lambda row: (datetime.datetime.strptime(row[0], '%Y-%m-%d'), float(row[1]), float(row[2])), data)]
+
+    x = [*map(itemgetter(0), data)]
+    means = [*map(itemgetter(1), data)]
+    medians = [*map(itemgetter(2), data)]
+    plt.plot(x, means, label="means")
+    plt.plot(x, medians, label="medians")
+    corrected = True
+
+plot_data(sys.argv[1])
+if len(sys.argv) > 2:
+    plot_data(sys.argv[2])
+
+plt.legend()
+plt.show()
+
