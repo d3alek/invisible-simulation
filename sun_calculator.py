Fixing a timezone related bug in the sky position calculation. Sky Map can now take a date argument (defaults to today)
The pysolar library has an archaic time zone management - get_altitude and get_azimuth
assume they are given the localized time, but not a UTC time with tzinfo attribute but
trully raw UTC time + DST value. So we do that and see that the days of DST change work better, and the sun position in the sky map looks correct in summer instead of -1 hour skewed.

sun_calculator.py
@@ -2,12 +2,14 @@ from pysolar.solar import *
 from pysolar.util import get_sunrise_sunset
 import datetime
 import numpy as np
+import pytz
 
 LATITUDE_SEVILLA_LONGITUDE_SEVILLA = (37.366123, -5.996422)
 
 LATITUDE_LONGITUDE_EDINBURGH = (55.9449353, -3.1839465)
 
 LATITUDE, LONGITUDE = LATITUDE_LONGITUDE_EDINBURGH
+TIMEZONE = 'Europe/London'
 
 def sunrise_sunset(when):
     return get_sunrise_sunset(LATITUDE, LONGITUDE, when)
@@ -16,6 +18,12 @@ def pysolar_to_local(pysolar_position):
     altitude, azimuth = pysolar_position
     return np.deg2rad(altitude), np.deg2rad((-azimuth + 180) % 360)
 
-def sun_position(datetime):
-    return pysolar_to_local((get_altitude(LATITUDE, LONGITUDE, datetime), get_azimuth(LATITUDE, LONGITUDE, datetime)))
+def sun_position(datetime_utc):
+    # The conversion from datetime_utc to raw_localized_datetime is necessary because
+    # get_altitude and get_azimuth assume the time is given in local time, but not
+    # the correct tzinfo way, that's why we add the DST value to the UTC time.
+    local_datetime = pytz.timezone(TIMEZONE).localize(datetime_utc)
+    raw_localized_datetime = datetime_utc + local_datetime.dst()
+
+    return pysolar_to_local((get_altitude(LATITUDE, LONGITUDE, localized_datetime), get_azimuth(LATITUDE, LONGITUDE, localized_datetime)))
 